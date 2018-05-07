c50efe7a56a39e573ea0a6ef1dfb378c4e0838f3
Alexander Jaust <alexander.jaust@uhasselt.be>
2018-05-07 10:36:20 +0200

    updated setup script for directories in .config

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
-dotfiles=(".gitignore_global" ".gitconfig" ".config")
+dotfiles=(".gitignore_global" ".gitconfig")
 dir="${HOME}/dotfiles"
 
 echo "Set dir to ${dir}"
 
 for dotfile in "${dotfiles[@]}";do
-  echo "Creating symlink for ${dotfile}"
+		echo "Creating symlink for ${dotfile}"
 #  echo  "${HOME}/${dotfile}"
 #  echo "${dir}/${dotfile}"
-  ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
+		ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
+done
+
+dotconfdirs=("i3")
+for dotconfdir in "${dotconfdirs[@]}";do
+		echo "Creating symlink for .config/${dotconfdir}"
+		rm -rf "${HOME}/.config/${dotconfdir}"
+		ln -sf "${dir}/${dotconfdir}" "${HOME}/.config/${dotconfdir}"		
 done
