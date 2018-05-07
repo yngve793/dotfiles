258ee1ad6c3fbf708b1522f4a2ed554e82d3136d
Alexander Jaust <alexander.jaust@uhasselt.be>
2018-05-07 12:09:40 +0200

    updated setup

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -12,6 +12,23 @@ for dotfile in "${dotfiles[@]}";do
 		ln -sf "${dir}/${dotfile}" "${HOME}/${dotfile}"
 done
 
+deploy_configs () {
+  echo "Deploy directories in .config"
+  appendname=$1
+  dotconfdir=$2
+  for dotconfdir in "${dotconfdirs[@]}";do
+		echo "Creating symlink for .config/${dotconfdir}"
+		rm -rf "${HOME}/.config/${dotconfdir}"
+		ln -sf "${dir}/${appendname}${dotconfdir}" "${HOME}/.config/${dotconfdir}"		
+  done
+}
+
+
+# System independent 
+dotconfdirs=("dunst")
+
+deploy_configs "" "${dotconfdirs}"
+
 dotconfdirs=("i3")
 
 appendname=""
@@ -20,12 +37,12 @@ case $HOSTNAME in
   (CRD-L-05716) appendname="linuxSubsystem/";;
   (*) appendname="";;
 esac
-
-
 echo "Set appendname to ${appendname}"
 
-for dotconfdir in "${dotconfdirs[@]}";do
-		echo "Creating symlink for .config/${dotconfdir}"
-		rm -rf "${HOME}/.config/${dotconfdir}"
-		ln -sf "${dir}/${appendname}${dotconfdir}" "${HOME}/.config/${dotconfdir}"		
-done
+deploy_configs "${appendname}" "${dotconfdirs}"
+
+#for dotconfdir in "${dotconfdirs[@]}";do
+#		echo "Creating symlink for .config/${dotconfdir}"
+#		rm -rf "${HOME}/.config/${dotconfdir}"
+#		ln -sf "${dir}/${appendname}${dotconfdir}" "${HOME}/.config/${dotconfdir}"		
+#done
